beae88b8d71daa11ecffd87c2bff12a3b63678c6
Ivan Shyshkarou <ivan.shyshkarou@sideways6.com>
2021-08-17 12:33:01 +0200

    Updated

---
+++ server/GameTheoryProject/Migrations/20210817065418_WinningNumberWereAdded.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameTheoryProject.Migrations
+{
+    public partial class WinningNumberWereAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WinningNumber",
+                table: "Games",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WinningNumber",
+                table: "Games");
+        }
+    }
+}
---
+++ server/GameTheoryProject/Migrations/20210817065618_AverageByHalfWereAdded.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameTheoryProject.Migrations
+{
+    public partial class AverageByHalfWereAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "AverageByHalf",
+                table: "Games",
+                type: "float",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageByHalf",
+                table: "Games");
+        }
+    }
+}
